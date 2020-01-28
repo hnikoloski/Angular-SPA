3fa490af53eada5f94fee38440ba917399fc38f4
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2020-01-28 11:37:45 +0100

    Fetch DB data

--- backend/app.js
+++ backend/app.js
@@ -37,21 +37,13 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [{
-            id: 'fid123123',
-            title: 'First Server side post',
-            content: 'Server side content'
-        },
-        {
-            id: '12erfdvs3',
-            title: 'Second Server side post',
-            content: 'Server side content.'
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetches succesfully.',
-        posts: posts
-    });
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Posts fetches succesfully.',
+                posts: documents
+            });
+        });
 });
 
 module.exports = app;
