e28b3a27ee58e5682918a86676b5a99beeffd14e
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2020-01-28 12:55:43 +0100

    typos and tweaks

--- backend/app.js
+++ backend/app.js
@@ -46,4 +46,9 @@ app.get('/api/posts', (req, res, next) => {
         });
 });
 
+app.delete("/api/posts/:id", (req, res, next) => {
+    console.log(req.params.id);
+    res.status(200).json({ message: "Post Deleted" })
+});
+
 module.exports = app;
