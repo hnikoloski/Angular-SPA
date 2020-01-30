40a0442a33cdb24fe44e5246090db51d8e5c0ef3
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2020-01-30 22:18:33 +0100

    Routing Added

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Post } from "./post.model";
+import { Post } from './post.model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
@@ -15,7 +15,7 @@ export class PostsService {
   getPosts() {
     this.http
       .get<{ message: string; posts: any }>(
-        "http://localhost:3000/api/posts"
+        'http://localhost:3000/api/posts'
       )
       .pipe(map((postData) => {
         return postData.posts.map(post => {
@@ -39,7 +39,7 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe(responseData => {
         const id = responseData.postId;
         post.id = id;
@@ -49,7 +49,7 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    this.http.delete("http://localhost:3000/api/posts/" + postId)
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
       .subscribe(() => {
         const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
